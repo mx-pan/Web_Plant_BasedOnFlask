b6cd076f5a18ddc36f7594ee2263845d94986837
mx-pan <392889690@qq.com>
2018-04-23 11:10:10 +0800

    第四次版本提交 - 20180423

--- server/flask_sqlite_init.py
+++ server/flask_sqlite_init.py
@@ -28,37 +28,31 @@ db.init_app(app)
 app.app_context().push()
 
 def add_data():
-    db.session.add(USERS('user1', 'test', '管理员', 'ALL',  '张三', '1120141234', '管理部门',
-                         '管理中心', '01', '13901234567'))
+    db.session.add(USERS('pmx', 'pmx', '管理员', 'ALL',  '潘淼鑫', '1120151488', '开发部门',
+                         '管理中心', '01', '18801234567'))
+    db.session.add(USERS('qyt', 'qyt', '管理员', 'ALL',  '瞿云涛', '1120151481', '开发部门',
+                         '管理中心', '01', '18801234567'))
+    db.session.add(USERS('mzl', 'mzl', '管理员', 'ALL',  '马占林', '1120151487', '开发部门',
+                         '管理中心', '01', '18801234567'))
+    db.session.add(USERS('lmn', 'lmn', '管理员', 'ALL',  '李曼妮', '1120151506', '开发部门',
+                         '管理中心', '01', '18801234567'))
+    db.session.add(USERS('cjc', 'cjc', '管理员', 'ALL',  '陈嘉诚', '1120151500', '开发部门',
+                         '管理中心', '01', '18801234567'))
+    db.session.add(USERS('user1', 'test', '养护人员', 'STATION',  '张三', '1120141234', '养护站',
+                         '养护站', '01', '13901234567'))
     db.session.add(USERS('user2', 'test', '养护人员', 'STATION',  '李四', '1120151234', '养护站',
                          '养护站', '01', '13901234567'))
     db.session.add(USERS('user3', 'test', '养护人员', 'MAINTAINER', '王五', '1120161234',
                          '养护站', '养护站', '02', '13901234567'))
 
-    db.session.add(MAIN_MENU('设备状态', 'StatusPage', "AGV及传感器状态",
-                             "mycolor1", "medkit", '[ALL], [STATION]', 'NONE'))
-    db.session.add(MAIN_MENU('物流状态', 'TansportPage', "物流传输状态",
-                             "mycolor2", "stats", '[ALL], [STATION]', '1'))
-    db.session.add(MAIN_MENU('收发物品', 'TansmitPage', "确认接收或发送物品",
-                             "mycolor3", "medical", '[ALL], [STATION]', '2'))
-    db.session.add(MAIN_MENU('系统控制', 'ControlPage', "AGV及物流控制",
-                             "mycolor4", "construct", 'ALL', 'NONE'))
-    db.session.add(MAIN_MENU('我', 'UserPage', "用户信息管理", "mycolor5",
-                             "person", '[ALL], [STATION],[MAINTAINER]', 'NONE'))
-    db.session.add(MAIN_MENU('设置', 'SettingsPage', "系统设置",
-                             "mycolor6", "hammer", 'ALL', 'NONE'))
-    db.session.add(MAIN_MENU('文档&帮助', 'DocumentPage', "系统介绍,操作指南",
-                             "mycolor1", "document", 'ALL', 'NONE'))
-    db.session.add(MAIN_MENU('数据库管理', 'DbManagerPage',
-                             "仅限工程人员使用。", "mycolor2", "analytics", 'ALL', '新'))
-    db.session.add(MAIN_MENU('服务器管理', 'ServerManagerPage',
-                             "仅限工程人员使用", "mycolor3", "outlet", 'ALL', 'NONE'))
-
-    db.session.add(SENSOR('AGV1条码扫描仪1', '01', "这个节点位于XXXX", '05-001',"20180317-01-001", "/file/assets/imgs/agv.png", '故障', '', "danger", '127.0.0.12', '8080'))
-    db.session.add(SENSOR('REID扫描仪1', '02', "这个节点位于XXXX", '05-001',"20180317-02-001", "/file/assets/imgs/agv.png", '正常', "", "secondary", '127.0.0.13', '8080'))
+    db.session.add(SENSOR('空气温度传感器', '01', "这个节点位于XXXX", '05-001',"20180419-01-001", "/file/assets/imgs/agv.png", '故障', '', "danger", '127.0.0.12', '8080'))
+    db.session.add(SENSOR('土壤湿度传感器', '02', "这个节点位于XXXX", '05-001',"20180419-02-001", "/file/assets/imgs/agv.png", '正常', "", "secondary", '127.0.0.13', '8080'))
 
     db.session.add(PLANTS('紫荆', '裸枝树，紫珠', "豆科", '紫荆属',"紫荆-001", "/file/assets/imgs/agv.png", '良好', '暖带树种，较耐寒。喜光，稍耐阴。喜肥沃、排水良好的土壤，不耐湿。', "??", '127.0.0.12', '8080'))
- 
+    db.session.add(PLANTS('樱花','东京樱花，日本樱花','蔷薇科','樱属',"樱花-001", "/file/assets/imgs/agv.png", '良好', '性喜阳光和温暖湿润的气候条件，有一定耐寒及抗旱能力，但对烟及风抗力弱。不耐酸碱土，根系较浅，忌积水洼地', "??", '127.0.0.12', '8080'))
+    db.session.add(PLANTS('西府海棠','无','蔷薇科','苹果属',"海棠-001", "/file/assets/imgs/agv.png", '良好', '喜光，耐寒，忌水涝，忌空气过湿，较耐旱', "??", '127.0.0.12', '8080'))
+    db.session.add(PLANTS('海棠花','无','蔷薇科','苹果属',"海棠-002", "/file/assets/imgs/agv.png", '良好', '性喜阳光，不耐阴，忌水湿，极为耐寒', "??", '127.0.0.12', '8080'))
+  
     db.session.commit()
 
 
